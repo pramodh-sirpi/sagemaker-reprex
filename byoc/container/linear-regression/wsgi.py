06c1f9b47b72f95977304556f0ce869263df3564
pramodh-sirpi <pramodh@sirpi.io>
2023-01-17 12:20:32 +0530

    added BYOC code

---
+++ byoc/container/linear-regression/wsgi.py
@@ -0,0 +1,11 @@
+import predictor as myapp
+
+# This is just a simple wrapper for gunicorn to find your app.
+# If you want to change the algorithm file, simply change "predictor" above to the
+# new file.
+
+app = myapp.app
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
